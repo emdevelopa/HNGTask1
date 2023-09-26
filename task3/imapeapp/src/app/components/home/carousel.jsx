e6b83cd00039dcc8a515cec80fd0ce7924e77203
emdevelopa <gyimahsunday@gmail.com>
2023-09-26 09:45:49 +0100

    morning commit-carousel

--- task3/imapeapp/src/app/components/home/carousel.jsx
+++ task3/imapeapp/src/app/components/home/carousel.jsx
@@ -1,6 +1,36 @@
-export default function Carousel(){
-    return(
+import Image from "next/image"
+import { useState } from "react"
+
+export default function Carousel({ images }) {
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    const nextSlide = () => {
+
+        setCurrentSlide((prevSlide) => {
+            prevSlide === images.length - 1 ? 0 : prevSlide + 1
+        })
+    }
+    const prevSlide = () => {
+        setCurrentSlide((prevSlide) => {
+            prevSlide === 0 ? images.length - 1 : prevSlide - 1
+        })
+    }
+    return (
         <>
+            <main className="bg-red-900 h-[30em] w-full overflow-hidden">
+                <div className="flex gap-4 transition-transform duration-500 ease-in-out" style={{
+                        transform: `translateX(-${currentSlide * 100}%)`,
+                    }}>
+                    {images.map((image, index) => (
+                        <div key={index} className="bg-blue-400 p-4 w-full flex flex-shrink-0 items-center" style={{ flexBasis: "100%" }}>
+                            <Image src={'/' + image.url} alt={'slider' + index} width={500} height={200} />
+                        </div>
+                    ))}
+
+                </div>
+                <button onClick={nextSlide}>Next</button>
+                <button onClick={prevSlide}>prev</button>
+            </main>
         </>
     )
 }
--- task3/imapeapp/src/app/components/home/home.jsx
+++ task3/imapeapp/src/app/components/home/home.jsx
@@ -1,4 +1,19 @@
 import Image from "next/image";
+import Carousel from "./carousel";
+
+const images = [
+    {
+        'url': 'nature4.png'
+    }, {
+        'url': 'nature2.png'
+    },
+    {
+        'url': 'nature3.png'
+    },
+    {
+        'url': 'nature1.png'
+    }
+]
 
 export default function Homepage() {
     return (
@@ -6,7 +21,7 @@ export default function Homepage() {
             <section className="p-4 flex">
                 <div className="flex gap-4 flex-col w-full rounded p-2 items-center">
                     <div className="flex gap-2 shadow-[0px_0px_3px_0px_grey] rounded p-2">
-                        <Image className="rounded" src='/nature1.png' alt="natureimg" height={0} width={250}/>
+                        <Image className="rounded" src='/nature1.png' alt="natureimg" height={0} width={250} />
                         <Image className="rounded" src='/nature3.png' alt="natureimg" height={0} width={250} />
 
                     </div>
@@ -17,7 +32,7 @@ export default function Homepage() {
                     </div>
                 </div>
                 <div className="w-full">
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit, placeat rerum provident ratione voluptates, dolor natus magni doloremque commodi, ut illo explicabo nisi tenetur quisquam ab. Voluptatem quod dolor quisquam!</p>
+                    <Carousel images={images} />
                 </div>
             </section>
         </>
