42fbb6358f66b8a6d2de8f5d1653f55999b0c6bf
emdevelopa <gyimahsunday@gmail.com>
2023-09-26 16:22:15 +0100

    Login page

--- task3/imapeapp/src/app/components/login/page.jsx
+++ task3/imapeapp/src/app/components/login/page.jsx
@@ -3,23 +3,26 @@ export default function Login() {
   return (
     <>
       <main className="flex items-center justify-center h-screen bg-[#0f0a2c] bg-blend-saturation">
-        <div className="h-[20em] bg-[#3d3d3d17] rounded border border-[#b5b5b584]">
-          <h1> SIGN IN</h1>
-          <form>
-            <div>
-              <label>Username</label>
-              <input type='text' placeholder="Email or Username" />
-            </div>
-            <div>
-              <label>Password</label>
-              <input type='text' placeholder="Password" />
-            </div>
-            <div>
-              <input type='submit' value='Log in' />
-            </div>
-          </form>
+        <div className="bg-[#3d3d3d59] rounded border border-[#b5b5b5ac] text-[#fff] px-8 py-12">
+          <div className=""> 
+            <h1 className="text-center font-bold text-[28px] mb-8"> SIGN IN</h1>
+            <form className="flex flex-col gap-6">
+              <div className="flex flex-col gap-2">
+                <label className="font-bold">Username</label>
+                <input type='text' className="bg-[#56565660] outline-none rounded-[2px] p-3" placeholder="Email or Username" />
+              </div>
+              <div className="flex flex-col gap-2">
+                <label className="font-bold">Password</label>
+                <input type='text' className="bg-[#56565660] outline-none rounded-[2px] p-3" placeholder="Password" />
+              </div>
+              <div className="mt-8">
+                <input type='submit' className="bg-[#ffffff] w-full outline-none rounded-[2px] p-3 text-black font-bold" value='Log in' />
+              </div>
+            </form>
+          </div>
         </div>
       </main>
+
     </>
   )
 }
