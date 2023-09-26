f16e8b90a17cc0a67f3f372d359400db1145624f
emdevelopa <gyimahsunday@gmail.com>
2023-09-26 12:11:18 +0100

    Login page

--- task3/imapeapp/src/app/components/login/page.jsx
+++ task3/imapeapp/src/app/components/login/page.jsx
@@ -2,8 +2,8 @@ export default function Login() {
 
   return (
     <>
-      <main className="flex items-center justify-center h-screen bg-black bg-blend-saturation">
-        <div className="h-[20em] bg-[#b3b3b32e] rounded">
+      <main className="flex items-center justify-center h-screen bg-[#0f0a2c] bg-blend-saturation">
+        <div className="h-[20em] bg-[#3d3d3d17] rounded border border-[#b5b5b584]">
           <h1> SIGN IN</h1>
           <form>
             <div>
