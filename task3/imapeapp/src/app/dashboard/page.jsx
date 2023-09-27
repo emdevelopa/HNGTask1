76c79113a2474f910646cbc23e47f4aa7d6fc445
emdevelopa <gyimahsunday@gmail.com>
2023-09-27 13:33:37 +0100

    Login page

--- task3/imapeapp/src/app/dashboard/page.jsx
+++ task3/imapeapp/src/app/dashboard/page.jsx
@@ -1,5 +1,4 @@
 "use client"
-
 export default function Dashboard() {
 
     const hasSessionStorageItem = sessionStorage.getItem('id');
@@ -13,7 +12,8 @@ export default function Dashboard() {
         <>
             <section className="flex flex-col items-center">
                 <div className="bg-blue-950 w-[20em] h-[20em] rounded-[50%] flex items-center justify-center p-6">
-                    <h1 className="text-center">DRAG AND DROP HERE</h1>
+                    <label id="file" className="text-center">DRAG AND DROP HERE</label>
+                    <input type="file" name="" id="file" />
                 </div>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 m gap-2 place-items-center">
                     <div className="w-[20em] h-[15em] rounded-lg bg-black"></div>
