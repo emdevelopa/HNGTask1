590eaff0902b29b7cee63aae5cf2136a631c5658
emdevelopa <gyimahsunday@gmail.com>
2023-09-27 06:39:33 +0100

    Login page

--- task3/imapeapp/src/app/dashboard/page.jsx
+++ task3/imapeapp/src/app/dashboard/page.jsx
@@ -2,7 +2,7 @@ export default function Dashboard(){
     return(
         <>
          <section>
-            <div className="bg-blue-950 w-[10em] h-[10em]">
+            <div className="bg-blue-950 w-[10em] h-[10em] rounded-[50%] flex">
                 <h1>DRAG AND DROP HERE</h1>
             </div>
          </section>
