b890571419a1cea979d827f686a5543aa9a58ace
emdevelopa <gyimahsunday@gmail.com>
2023-09-28 08:45:58 +0100

    nice

--- task2/movieBox/src/app/sections/header/header.jsx
+++ task2/movieBox/src/app/sections/header/header.jsx
@@ -21,6 +21,7 @@ export default function Header() {
         console.error("Error fetching trending movies:", error);
       }
     };
+    console.log(hello);
 
     fetchTrendingMovies();
   }, [apiUrl]);
